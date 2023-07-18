0a0f040de95b776935bd9d242c2ca3db7cc41b6d
Ing. Nelson Gonzalez Escalante <51499655+nescalan@users.noreply.github.com>
2023-07-18 15:21:04 -0600

    Update Connection
    + Password

--- app/model/Connection.php
+++ app/model/Connection.php
@@ -5,7 +5,7 @@ class Connection
     # Properties 
     protected $host = 'localhost'; // Host name or IP address of the database server
     protected $user = 'root'; // Username to access the database with read/write privileges
-    protected $password = ''; // Password for that user account
+    protected $password = '4u3p7px6'; // Password for that user account
     protected $db = 'jobtracker'; // Name of the database we are connecting to
 
     # Create connection
