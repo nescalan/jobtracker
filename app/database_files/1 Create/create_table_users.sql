d30e6d4a6ad86f7743a79edc89342f00a3ad9dee
Ing. Nelson Gonzalez Escalante <51499655+nescalan@users.noreply.github.com>
2023-07-27 09:36:36 -0600

    Update create_table_users.sql
    + added a foreign key company_id, links this table to compnaies table

--- app/database_files/1 Create/create_table_users.sql
+++ app/database_files/1 Create/create_table_users.sql
@@ -2,6 +2,8 @@ CREATE TABLE users(
 	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     fullname VARCHAR(100) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    PRIMARY KEY (id)
+    company_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    user_password VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_company_id_users FOREIGN KEY(company_id) REFERENCES companies(id)
 )ENGINE = InnoDB;
