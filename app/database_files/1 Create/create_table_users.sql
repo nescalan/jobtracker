3ab366a029b743a682bf142f5e95a079e10a37b0
Ing. Nelson Gonzalez Escalante <51499655+nescalan@users.noreply.github.com>
2023-07-27 10:19:31 -0600

    Update create_table_users.sql
    Updated: company_id INT UNSIGNED NOT NULL,

--- app/database_files/1 Create/create_table_users.sql
+++ app/database_files/1 Create/create_table_users.sql
@@ -2,7 +2,7 @@ CREATE TABLE users(
 	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     fullname VARCHAR(100) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    company_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    company_id INT UNSIGNED NOT NULL,
     user_password VARCHAR(255) NOT NULL,
     PRIMARY KEY (id),
     CONSTRAINT fk_company_id_users FOREIGN KEY(company_id) REFERENCES companies(id)
