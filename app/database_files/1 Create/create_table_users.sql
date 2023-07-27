ba30f81f1a90bb7041a3b418b89e66152448ff48
Ing. Nelson Gonzalez Escalante <51499655+nescalan@users.noreply.github.com>
2023-07-27 16:53:26 -0600

    Update

--- app/database_files/1 Create/create_table_users.sql
+++ app/database_files/1 Create/create_table_users.sql
@@ -2,8 +2,8 @@ CREATE TABLE users(
 	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     fullname VARCHAR(100) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    company_id INT UNSIGNED NOT NULL,
+    affiliated_company_id INT UNSIGNED NOT NULL,
     user_password VARCHAR(255) NOT NULL,
     PRIMARY KEY (id),
-    CONSTRAINT fk_company_id_users FOREIGN KEY(company_id) REFERENCES companies(id)
+    CONSTRAINT fk_affiliated_company_id_users FOREIGN KEY(affiliated_company_id) REFERENCES affiliated_companies(id)
 )ENGINE = InnoDB;
