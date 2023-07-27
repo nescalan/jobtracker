e7fb148e348cee960e5ee279ffc0a731bc77e889
Ing. Nelson Gonzalez Escalante <51499655+nescalan@users.noreply.github.com>
2023-07-27 17:08:25 -0600

    Update

--- app/database_files/1 Create/create_table_articles.sql
+++ app/database_files/1 Create/create_table_articles.sql
@@ -1,5 +1,4 @@
-CREATE TABLE
-    articles (
+CREATE TABLE articles (
         id INT UNSIGNED NOT NULL AUTO_INCREMENT,
         description_id INT UNSIGNED NOT NULL,
         model_brand_id INT UNSIGNED NOT NULL,
@@ -7,5 +6,5 @@ CREATE TABLE
         serial_number VARCHAR(45) NOT NULL,
         PRIMARY KEY (id),
         CONSTRAINT fk_description_id_articles FOREIGN KEY (description_id) REFERENCES article_descriptions (id),
-        CONSTRAINT fk_model_brand_id_articles FOREIGN KEY (model_brand_id) REFERENCES models_brands (model_brand_id)
+        CONSTRAINT fk_model_brand_id_articles FOREIGN KEY (model_brand_id) REFERENCES models_brands (id)
     ) ENGINE = InnoDB;
